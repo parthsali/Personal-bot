44ad59573f15e543b7aaf06a077d2e1b0b9a7d4b
parthsali <parthsali04@gmail.com>
2025-08-07 23:29:57 +0530

    feat: add github_service

--- app/main.py
+++ app/main.py
@@ -1,2 +1,12 @@
+from services.github_service import GitHubService
 from config import Config
+import json
 
+github_service = GitHubService(Config.GITHUB_PERSONAL_ACCESS_TOKEN)
+
+data = github_service.fetch_all_detailed_repos()
+
+file_path = "data.json"
+
+with open(file_path, 'w') as json_file:
+    json.dump(data, json_file, indent=4)
